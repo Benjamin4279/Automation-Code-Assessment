f781363ed0edfa32c357aa285947c5d50eb69ac8
Benjamin4279 <batmanblue79@gmail.com>
2019-04-10 13:37:43 -0400

    Add files via upload

---
+++ features.js
@@ -0,0 +1,63 @@
+1. Scenario: Calculation display
+
+	Given('I view the calculator screen', function(){
+		browser.get("https://www.exercise1.com/values");
+	});
+
+	When('I view each amount that corresponds to the values', function(){
+		browser.get("https://www.exercise1.com/values");
+	
+	});
+	
+	Then('I should see the values appear', function(){
+		browser.get("https://www.exercise1.com/values");
+	});
+
+2. Scenario: Verify the values on the screen is greater than zero
+
+	Given('I view the calculator screen', function(){
+		browser.get("https://www.exercise1.com/values");
+	});
+	
+	When('I view each amount that corresponds to the values', function(){
+		brower.get("https://www.exercise1.com");
+	});
+	Then('I should see the values appear', function(){
+		browser.get("https://www.exercise1.com");
+	});
+
+3. Scenario: Verify the total balance is correct
+
+	Given('I view the calculator screen', function(){
+		browser.get("https://www.exercise1.com");
+	});
+	When('I view each amount that corresponds to the values', function(){
+		browser.get("https://www.exercise1.com");
+	});
+	Then('I should see the values appear', function(){
+		browser.get("https://www.exercise1.com");
+	});
+
+4. Scenario: Verify the values are formatted as currencies
+
+	Given('I view the calculator screen', function(){
+		browser.get("https://www.exercise1.com");
+	});
+	When('I view each amount that corresponds to the values', function(){
+		browser.get("https://www.exercise1.com");
+	});
+	Then('I should see the values appear', function(){
+		browser.get("https://www.exercise1.com");
+	});
+	
+5. Scenario: Verify the total balance matches sum of the values
+
+	Given('I view the calculator screen', function(){
+		browser.get("https://www.exercise1.com");
+	});
+	When('I view each amount that corresponds to the values', function(){
+		browser.get("https://www.exercise1.com");
+	});
+	Then('I should see the values appear', function(){
+		browser.get("https://www.exercise1.com");
+	});
